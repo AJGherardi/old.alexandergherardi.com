d2c36c762165f2cd6d8d8b719964206ca1718526
Alexander Gherardi <alexander.2003@live.com>
2021-10-29 01:38:36 -0400

    Add LinkedIn

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ let desc1 = document.getElementsByClassName('line1')[0];
 let desc2 = document.getElementsByClassName('line2')[0];
 let desc3 = document.getElementsByClassName('line3')[0];
 let github = document.getElementById('github');
+let linkedin = document.getElementById('linkedin');
 
 name.innerHTML = name.textContent.replace(/\S/g, "<span class='nameLetter'>$&</span>");
 desc1.innerHTML = desc1.textContent.replace(/\S/g, "<span class='desc1Letter'>$&</span>");
 desc2.innerHTML = desc2.textContent.replace(/\S/g, "<span class='desc2Letter'>$&</span>");
 desc3.innerHTML = desc3.textContent.replace(/\S/g, "<span class='desc3Letter'>$&</span>");
 github.innerHTML = github.textContent.replace(/\S/g, "<span class='githubLetter'>$&</span>");
+linkedin.innerHTML = linkedin.textContent.replace(/\S/g, "<span class='linkedinLetter'>$&</span>");
 
 
 anime.timeline()
@@ -41,6 +43,13 @@ anime.timeline()
     })
     .add({
         targets: 'a .githubLetter',
+        opacity: [0, 1],
+        easing: "easeInOutElastic",
+        offset: 0,
+        delay: (el, i) => 75 * (i + 1)
+    })
+    .add({
+        targets: 'a .linkedinLetter',
         opacity: [0, 1],
         easing: "easeInOutElastic",
         offset: 0,
